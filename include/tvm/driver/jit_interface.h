dbcf2e47eff4b818c08f972e5d351acdc4394615
Jared Roesch <jroesch@octoml.ai>
2021-03-05 14:02:38 -0800

    Fix

--- include/tvm/driver/jit_interface.h
+++ include/tvm/driver/jit_interface.h
@@ -4,9 +4,6 @@
 extern "C" {
     EXPORT_DLL tvm::runtime::Module TVMCompile(const std::string& onnx_txt, const std::string& target, const std::string& target_host, int opt_level, const std::vector<std::vector<int64_t>>& input_shapes);
     EXPORT_DLL void TVMExtractOutputShapes(tvm::runtime::Module& mod, size_t num_outputs, std::vector<std::vector<int64_t>>& output_shapes);
-
-    EXPORT_DLL void TVMRun(tvm::runtime::Module& mod, std::vector<DLTensor>& inputs, std::vector<DLTensor>& outputs);
-    
-    
+    EXPORT_DLL void TVMRun(tvm::runtime::Module& mod, std::vector<DLTensor>& inputs, std::vector<DLTensor>& outputs, tvm::runtime::TVMRetValue *ret);
 }  // TVM_EXTERN_C
 #endif
